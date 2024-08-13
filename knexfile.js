aad7bb5a99c7f5286beba35d6ccfced22009f469
Joseph Aizawa <josephaizawa@gmail.com>
2024-08-13 11:36:05 -0700

    updates knex file and env for database deployment

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,10 @@ dotenv.config();
 export default {
   client: "mysql2",
   connection: {
-    host: "127.0.0.1",
-    user: "root",
-    password: "rootroot",
-    database: "dnd",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     charset: "utf8",
   },
 };
